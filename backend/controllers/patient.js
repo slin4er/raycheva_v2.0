c0e30f9c8239e70e18d6fd5a1cb885211531293b
slin4er <slin4eria@gmail.com>
2022-12-05 00:57:21 +0100

    check for email

--- backend/controllers/patient.js
+++ backend/controllers/patient.js
@@ -5,7 +5,9 @@ const sendEmail = require('../emails/email')
 //CRUD FOR PATIENTS
 const createPatient = async (req, res) => {
     const patient = await Patient.create(req.body)
-    sendEmail(patient, `Спасибо, ${patient.name}, что записались ко мне на прием, напоминаю, что вы должны прийти ${patient.appointment} в здесь будет время`)
+    if(patient.email) {
+        sendEmail(patient, `Спасибо, ${patient.name}, что записались ко мне на прием, напоминаю, что вы должны прийти ${patient.appointment} в здесь будет время`)
+    }
     res.status(201).json({patient})
 }
 
@@ -26,7 +28,9 @@ const updatePatient = async (req, res) => {
     const {id: patient_id} = req.params
     const patient = await Patient.findByIdAndUpdate(patient_id, req.body, {new: true})
     if(!patient) {throw new Error('Not Found')}
-    sendEmail(patient, `Спасибо, ${patient.name}, что записались ко мне на прием, напоминаю, что ваша запись была перенесена на ${patient.appointment} в здесь будет время`)
+    if(patient.email) {
+        sendEmail(patient, `Спасибо, ${patient.name}, что записались ко мне на прием, напоминаю, что ваша запись была перенесена на ${patient.appointment} в здесь будет время`)
+    }
     res.status(201).json({patient})
 }
 
