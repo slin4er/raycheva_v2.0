b4e63ef378bb4d878811f0ecc0586a798d93d3ff
Gleb Gurgurov <gurragyrra@gmail.com>
2023-02-06 19:18:36 +0200

    new laptop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 export const Header: FC = () => {
 	return (
 		<HeaderBlock>
-			<Label>Райчева Эвелина Николаевна</Label>
+			<Label>Райчева Эвелина Николаевна NEW LAPTOP</Label>
 		</HeaderBlock>
 	)
 }
